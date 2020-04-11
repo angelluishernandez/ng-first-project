282ce4715896e58216980ac0bf14cdfcb8de19bd
Ángel Luis Hernández Herrero <angelluishernandezherrero@gmail.com>
2020-04-11 17:55:08 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,29 @@
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { HeaderComponent } from './header/header.component';
+import { RootComponent } from './root/root.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipesComponent } from './recipes/recipes.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeListComponent,
+    RootComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipesComponent,
   ],
   imports: [
     BrowserModule,
